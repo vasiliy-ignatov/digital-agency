e589e9e13a68cb1b2e42ef8662d3f208892cb95f
Artem Simanov <metall704@yandex.ru>
2022-05-08 15:56:46 +0300

    add header, menu, intro+swiper

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,20 +1,10 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
-    spaceBetween: 20,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+    spaceBetween: 20,     
     
-    breakpoints: {
-        // when window width is >= 320px
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 10
-        }
-    }
-  });
+    pagination: {
+      el: '.swiper-pagination',
+    },  
+});
