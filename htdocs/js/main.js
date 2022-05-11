f858c52ebe68596ae232f63d604e3ac60af57a76
Artem Simanov <metall704@yandex.ru>
2022-05-11 21:17:48 +0300

    add services script

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -8,3 +8,27 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-pagination',
     },  
 });
+
+let servicesItems = document.querySelectorAll('.services__item');
+
+for (servicesItem of servicesItems) {
+  let blockText = servicesItem.querySelector('.block');
+
+  let serviceDescription = document.createElement('p');
+  serviceDescription.textContent = "Создадим бла-бла-бла";
+  serviceDescription.classList.add('block__text');
+
+  let serviceAbout = document.createElement('p');
+  serviceAbout.textContent = 'Подробнее';
+  serviceAbout.classList.add('service-about');
+  
+  servicesItem.addEventListener('mouseover', function() {    
+    blockText.appendChild(serviceDescription);
+    blockText.appendChild(serviceAbout);
+  })
+
+  servicesItem.addEventListener('mouseout', function() {
+    serviceDescription.remove();
+    serviceAbout.remove();
+  })
+}
